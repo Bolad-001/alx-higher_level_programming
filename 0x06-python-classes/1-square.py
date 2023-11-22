df919483ab5986d540f96a92af1d86ca23f80f21
Bolad <adebayoabubakar44@gmail.com>
2023-11-22 12:15:06 -0800

    commit task 1 and 2

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+""" Define a class Square """
+
+class Square:
+    """ class represent a square """
+
+    def __init__(self, size):
+        """ initialize a new square.
+        Args:
+            size: size of the new square.
+        """
+        self.__size = size
