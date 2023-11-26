d8431ad218df1a20862e944d0f40be407888eaa4
Bolad <adebayoabubakar44@gmail.com>
2023-11-26 11:08:18 -0800

    commit task

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+"""
+    Function that print square
+"""
+def print_square(size):
+    """
+        print square with char "#"
+
+    Args:
+        size: size of square
+    """
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+    if isinstance(size, float):
+        raise TypeError("size must be an integer")
+
+    if size < 0:
+        raise ValueError("size must be >= 0")
+    for i in range(size):
+        print("#" * size)
+
