f3e6127251edc47d9cc38eea7478ab9101826282
Bolad <adebayoabubakar44@gmail.com>
2023-12-12 09:03:45 -0800

    fix error

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -75,8 +75,8 @@ class Rectangle(Base):
             print("#" * self.width)
 
     def __str__(self):
-        return (f"[Rectangle] ({self.id})\
-                {self.x}/{self.y} - {self.width}/{self.height}")
+        return (f"[Rectangle] ({self.id})"
+                f" {self.x}/{self.y} - {self.width}/{self.height}")
 
     def update(self, *args, **kwargs):
         """ Assign attribute """
